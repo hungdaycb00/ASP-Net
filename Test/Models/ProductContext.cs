4142709ccb5df3c94668eaa03f63835b8dcf488f
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-10-29 20:49:17 +0700

    .

---
+++ Test/Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class ProductContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
