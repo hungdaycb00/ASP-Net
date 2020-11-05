9f63e4b8923b8c2dc45bf05ef10b2e885b0d088d
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-11-05 17:38:21 +0700

    mvc

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: HelloWorld
+
+        //public string Welcome()
+        //{
+
+        //    return "welcome";
+        //}
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+        public ActionResult Welcome(string name, int id = 1)
+        {
+            ViewBag.Message = "Hello " + name + " <3";
+            ViewBag.NumTimes = id;
+                return View();
+        }
+    }
+}
