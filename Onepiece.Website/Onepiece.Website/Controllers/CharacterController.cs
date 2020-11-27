8ac7a6c3f96150aebc02a8ff7b0154fa90eba5ee
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-11-27 10:08:29 +0700

    ASM ASp.net Core Application

---
+++ Onepiece.Website/Onepiece.Website/Controllers/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Onepiece.Website.Models;
+using Onepiece.Website.Services;
+
+namespace Onepiece.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        public CharacterController(JsonFileCharacterService characterService)
+        {
+            this.CharacterService = characterService;
+        }
+        public JsonFileCharacterService CharacterService { get; }
+
+        [HttpGet]
+        public IEnumerable<Character> Get()
+        {
+            return CharacterService.GetCharacters();
+        }
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string CharacterId, [FromQuery] int Rating)
+        {
+            CharacterService.AddRating(CharacterId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ Onepiece.Website/Onepiece.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Onepiece.Website.Models;
+using Onepiece.Website.Services;
+
+namespace Onepiece.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileCharacterService CharacterService;
+
+        public IEnumerable<Character> Characters { get; private set; }
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileCharacterService characterService)
+        {
+            _logger = logger;
+            CharacterService = characterService;
+        }
+
+        public void OnGet()
+        {
+            Characters = CharacterService.GetCharacters();
+        }
+    }
+}
