38779aca6ba9f5c9a3d514c121746953cd6821fc
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-12-01 09:08:49 +0700

    .

---
+++ ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.NongPhanManhHung.Models;
+using ProductManager.NongPhanManhHung.Services;
+
+namespace ProductManager.NongPhanManhHung.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductServices productService)
+        {
+            this.ProductServices = productService;
+        }
+        public JsonFileProductServices ProductServices { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductServices.GetProducts();
+        }
+    }
+}
--- ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Startup.cs
+++ ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Startup.cs
@@ -27,6 +27,7 @@ namespace ProductManager.NongPhanManhHung
             services.AddRazorPages();
             services.AddTransient<JsonFileProductServices>();
             services.AddServerSideBlazor();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +57,7 @@ namespace ProductManager.NongPhanManhHung
             {
                 endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
+                endpoints.MapControllers();
             });
         }
     }
