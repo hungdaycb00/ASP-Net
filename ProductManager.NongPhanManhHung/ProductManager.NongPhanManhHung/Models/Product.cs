55d422a252e5d666f66d331c015998393849ccd9
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-12-01 08:33:34 +0700

    .

---
+++ ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProductManager.NongPhanManhHung.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
---
+++ ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ProductManager.NongPhanManhHung.Models;
+using ProductManager.NongPhanManhHung.Services;
+
+namespace ProductManager.NongPhanManhHung.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public JsonFileProductServices ProductService;
+
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductServices productServices)
+        {
+            _logger = logger;
+            ProductService = productServices;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
