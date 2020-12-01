e9a85b959d721902abbf01c020fad5e82af3446c
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-12-01 08:49:07 +0700

    .

--- ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Models/Product.cs
+++ ProductManager.NongPhanManhHung/ProductManager.NongPhanManhHung/Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -13,6 +15,9 @@ namespace ProductManager.NongPhanManhHung.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
     }
